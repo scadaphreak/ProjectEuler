ef4ed5c414a56844aa49c11a80cf1d641a8258a1
scadaphreak <scadaphreak@gmail.com>
2023-01-29 00:11:19 -0500

    More C++ Solutions

---
+++ CPP/001/cmake-build-debug/CMakeFiles/001.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "001"
+  "001.pdb"
+  "CMakeFiles/001.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/001.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
